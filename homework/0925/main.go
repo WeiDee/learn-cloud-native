882cf822b35f8e1fe3ce45024a809b1744cb2813
wee <dobbyM0324@163.com>
2021-09-30 13:14:02 +0800

    feat(homework):0925

--- homework/0925/main.go
+++ homework/0925/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -8,39 +9,62 @@ import (
 	"strings"
 )
 
+type logWriter struct {
+	http.ResponseWriter
+	*http.Request
+	statusCode int
+}
+
+func (lg *logWriter) writeHeader(code int) {
+	lg.statusCode = code
+	lg.ResponseWriter.WriteHeader(code)
+}
+
+func (lg *logWriter) writeLog() {
+	fmt.Fprintf(os.Stdout, "Client IP is: %s\n", lg.Request.RemoteAddr)
+	fmt.Fprintf(os.Stdout, "Respons code is: %d\n", lg.statusCode)
+}
+
+func newLogWriter(w http.ResponseWriter, req *http.Request) *logWriter {
+	return &logWriter{w, req, http.StatusOK}
+}
+
 func main() {
-	//resp with req header
-	respWithHeader := func(resp http.ResponseWriter, req *http.Request) {
+	//resp with req header and log
+	respWithHeader := func(w http.ResponseWriter, req *http.Request) {
 		httpHeader := req.Header
 
 		for k, v := range httpHeader {
-			resp.Header().Set(k, strings.Join(v, ","))
+			w.Header().Set(k, strings.Join(v, ","))
 		}
 
-		io.WriteString(resp, "This is resp with request header!\n")
+		lg := newLogWriter(w, req)
+		lg.writeLog()
+
+		io.WriteString(w, "This is resp with request header!\n")
 	}
 
 	//resp ENV version
-	version := func(resp http.ResponseWriter, req *http.Request) {
+	version := func(w http.ResponseWriter, req *http.Request) {
 
 		Key := "VERSION"
 
 		v := os.Getenv(Key)
 
-		resp.Header().Set(Key, v)
+		w.Header().Set(Key, v)
 
-		io.WriteString(resp, "This is resp with ENV VERSION!\n")
+		io.WriteString(w, "This is resp with ENV VERSION!\n")
 	}
 
 	//resp 200
-	healthz := func(resp http.ResponseWriter, req *http.Request) {
-		resp.WriteHeader(http.StatusOK)
-		io.WriteString(resp, "ok")
+	healthz := func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "ok")
 	}
 
 	http.HandleFunc("/", respWithHeader)
 	http.HandleFunc("/version", version)
 	http.HandleFunc("/healthz", healthz)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
